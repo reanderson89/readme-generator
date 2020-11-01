1b5d107775d230ad4756cc5fffe518368961df19
Robert Anderson <reanderson89@gmail.com>
2020-11-01 13:43:14 -0700

    added if/else statements inside of the template literal that allows for badges to be added to the readme based off of the users response.

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # **ReadMe Generator**
-
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
 ## **Table of Contents**
 
 
 ## **Live Link**
-
+${data.license['Apache 2.0'] ? 
+    `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+: ``}
 
 ## **Description**
 
--- index.js
+++ index.js
@@ -32,12 +32,7 @@ inquirer.prompt([
       {
         type: 'input',
         name: 'contribute',
-        message: 'Would you like others to contribute? If yes type "## **Contribute**" then hit enter, if no then leave blank.',
-      },
-      {
-        type: 'input',
-        name: 'contributeMessage',
-        message: 'If you selected yes to have others contribute, enter your stipulations here. If not, leave blank.',
+        message: 'What guidelines for contributing would you like?',
       },
       {
         type: 'input',
@@ -65,21 +60,38 @@ inquirer.prompt([
         message: 'What is your name?',
       },
       {
-        type: 'input',
+        type: 'list',
         name: 'license',
-        message: 'Add licensing.',
+        message: 'Choose your preferred licensing.',
+        choices: ['MIT','Apache 2.0','Mozilla Public License 2.0','The Do What the Fuck You Want to Public License']
       },
 
 ]).then((data) => {
 
 const readMeTemplate = `
 # **${data.title}**
-
+${
+    (license => { 
+        if(license === "Apache 2.0") 
+            return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        else if(license === "MIT")
+            return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+        else if(license === "Mozilla Public License 2.0")
+            return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+        else if(license === "The Do What the Fuck You Want to Public License")
+            return `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`
+        else
+            return ``
+    })(data.license) 
+}
 
 ## **Table of Contents**
+* [Contributors](#contributors)
+* [Demo](#demo)
 * [Installation](#installation)
 * [Usage](#usage)
-* [Demo](#demo)
+* [Tests](#tests)
+* [Questions](#questions)
 * [License](#license)
     
 ## **Live Link**
@@ -89,20 +101,20 @@ ${data.website}
 ${data.description}
     
  ## **Contributors**
-    * [${data.name}](https://github.com/${data.github})
-    <!--- (Delete these or add more as needed)-->
-    * [Enter other teammate](https://github.com/their-GitHub-Username)
+* [${data.name}](https://github.com/${data.github})
+<!--- (Delete these or add more as needed)-->
+* [Enter other teammate](https://github.com/their-GitHub-Username)
     
-    * [Enter other teammate](https://github.com/their-GitHub-Username)
+* [Enter other teammate](https://github.com/their-GitHub-Username)
     
-    * [Enter other teammate](https://github.com/their-GitHub-Username)
+* [Enter other teammate](https://github.com/their-GitHub-Username)
 
 ## **Technology Stack**
 ${data.tech}
     
 ## **Demo**
-![demo gif](pathway to gif goes here)
-![alt text](http://url/to/img.png or link from relative pathway)
+- ![demo gif](pathway to gif goes here)
+- ![alt text](http://url/to/img.png or link from relative pathway)
 
 ## **Installation**
 ${data.install}
@@ -115,15 +127,16 @@ ${data.tests}
     
 ## **Questions**   
 ####    **For any questions or inquiries please contact me at,**
-* #### **Name:** ${data.name}
+* #### **GitHub:** [@${data.github}](https://github.com/${data.github})
 * #### **Email:** ${data.email}
 
+## **Contribute**
 ${data.contribute}
-${data.contributeMessage}
-    
+  
 ## **License**
 ${data.license}
     `
+
 // creates name of readme
 const fileName = `${data.name.toLowerCase().split(" ").join('-')}.md`;
 
